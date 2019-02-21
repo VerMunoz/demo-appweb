4c6b5625d01304f63b25389ed51d0e2b49349f04
VerMunoz <vero_munoz08@hotmail.com>
2019-02-21 15:37:59 -0600

    Deploy demo

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ type Welcome struct {
 }
 
 func main() {
-	welcome := Welcome{" infra ctin :D  ", time.Now().Format(time.Stamp)}
+	welcome := Welcome{" infra ctin ", time.Now().Format(time.Stamp)}
 
 	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
 	http.Handle("/static/", //final url can be anything
